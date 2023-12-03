21347d66d0f6dd637968635e2392806130a3fdc8
AMBUJ KUMAR <90747841+fantom787@users.noreply.github.com>
2023-12-03 23:36:22 +0530

    demo changes

--- server.js
+++ server.js
@@ -26,6 +26,9 @@ nodeCron.schedule("0 0 0 1 * *", async () => {
   }
 });
 
+/*
+some changes
+*/
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
